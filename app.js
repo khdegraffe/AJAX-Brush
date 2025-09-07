a61955c2cd57e9265873b81211210315963a04d7
khdegraffe <khdegraffe@gmail.com>
2025-09-07 14:48:00 -0400

    add Swapi function

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("I'm CONNECTED");
+// fetch("https://swapi.tech/api/people/1/")
+//   .then((res) => {
+//     console.log("RESOLVED!!", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data.result.properties);
+//   })
+//   .catch((e) => {
+//     console.log("ERROR!", e);
+//   });
+
+const loadStarWarsPeople = async () => {
+  try {
+    const res = await fetch("https://swapi.tech/api/people/1/");
+    const data = await res.json();
+    console.log(data.result.properties);
+    const res2 = await fetch("https://swapi.tech/api/people/2/");
+    const data2 = await res2.json();
+    console.log(data2.result.properties);
+  } catch (e) {
+    console.log("ERROR!!!", e);
+  }
+};
+
+loadStarWarsPeople();
